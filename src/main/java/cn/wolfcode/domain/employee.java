69029fa8f2ed6e427c96c4f0940071f0177c32b0
keekobaby <910384926@qq.com>
2024-02-09 19:28:30 +0800

    print test2

--- src/main/java/cn/wolfcode/domain/employee.java
+++ src/main/java/cn/wolfcode/domain/employee.java
@@ -3,5 +3,6 @@ package cn.wolfcode.domain;
 public class employee {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test2");
     }
 }
